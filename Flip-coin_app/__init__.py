7cc6537c327c45728adb568229506826e94fd0cd
ODWANOMBACA <Odwanombaca18@gmail.com>
2023-12-26 16:36:14 +0000

    authentication registered in flask app

--- Flip-coin_app/__init__.py
+++ Flip-coin_app/__init__.py
@@ -7,7 +7,9 @@ def create_app():
     
     # Registering all blueprints
     from .views import views
+    from .auth import auth
 
     app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
 
     return app
