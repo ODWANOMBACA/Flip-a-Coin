6ef8737cfbbb93f2279ee44ef645b97f858ad7f3
ODWANOMBACA <Odwanombaca18@gmail.com>
2023-12-27 07:37:33 +0000

    app registered

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
-from flask import Flask
+from website import create_app
 
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Welcome to flip a coin!"
+app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
