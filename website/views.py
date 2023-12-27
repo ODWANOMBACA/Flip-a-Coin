1a5bdadbdd5a2365bfcf876b534eb27a2bb1f138
ODWANOMBACA <Odwanombaca18@gmail.com>
2023-12-27 14:30:48 +0000

    Route for flip coin created

--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import login_required, current_user
+import random
 
 views = Blueprint('views', __name__)
 
@@ -8,3 +9,19 @@ views = Blueprint('views', __name__)
 @login_required
 def home():
     return render_template("home.html", user=current_user)
+
+@views.route('/flip_coin')
+@login_required
+def flip_coin():
+    return render_template('flip_coin.html', user=current_user)
+
+@views.route('/flip_result', methods=['POST'])
+@login_required
+def flip_result():
+    heads = request.form.get('heads')
+    tails = request.form.get('tails')
+
+    # Add your coin flip logic here (randomly choose heads or tails)
+    result = "Heads" if random.choice([True, False]) else "Tails"
+
+    return render_template('flip_coin.html', result=result, heads=heads, tails=tails, user=current_user)
